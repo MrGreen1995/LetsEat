9b534030dfedf3e3f882d473dd61eaa16ec1dbb0
MrGreen1995 <mardonov2014@gmail.com>
2018-12-07 22:38:17 +0500

    DECEMBER 7 2018 RecyclerView Menu Item

--- app/src/main/java/com/example/odiljon/letseat/SignIn.java
+++ app/src/main/java/com/example/odiljon/letseat/SignIn.java
@@ -1,6 +1,7 @@
 package com.example.odiljon.letseat;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.odiljon.letseat.Common.Common;
 import com.example.odiljon.letseat.Model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -61,6 +63,11 @@ public class SignIn extends AppCompatActivity {
                             User user = dataSnapshot.child(phoneEdit.getText().toString()).getValue(User.class);
                             if (user.getPassword().equals(passwordEdit.getText().toString())) {
                                 Toast.makeText(SignIn.this, "Sign In Successfully", Toast.LENGTH_SHORT).show();
+                                Intent homeIntent = new Intent(SignIn.this, Home.class);
+                                Common.currentUser = user;
+                                startActivity(homeIntent);
+                                finish();
+
                             } else {
                                 Toast.makeText(SignIn.this, "Wrong Password !!!", Toast.LENGTH_SHORT).show();
                             }
